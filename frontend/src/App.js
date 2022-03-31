129147a426ca235d5c51d209ef4e0dbb3232b13b
Sok Den <sokdenspace@gmail.com>
2022-04-01 00:12:48 +0700

    navbar & grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,52 @@
-import './App.css';
-import React from 'react';
-import bg_image from './images/bg.jpg'
+import "./App.css";
+import React from "react";
+import logo from "../src/images/logo.png";
+
+console.log(logo); // /logo.84287d09.png
 
 function App() {
   return (
-    <div className='bg-image'>
-      <div className='text'>
+    <div class="containers">
+      <div class="navbar">
         <ul>
-          <h6>Welcome to Login page</h6><br />
-          <h6>Enter email</h6><br />
-          <h6>Enter password</h6><br />
-          <h6>Forgot password</h6>
+          <a href="http://localhost:3000/">
+            <img src={logo} alt="Logo" />
+          </a>
+          <li>
+            <a href="#Home">Home</a>
+          </li>
+          <li>
+            <a href="#About">About</a>
+          </li>
+          <li>
+            <a href="#Cart">Cart</a>
+          </li>
+          <li>
+            <a href="#Profile">Profile</a>
+          </li>
+          <li>
+            <a href="#Contact">Contact</a>
+          </li>
+          <input type="text" placeholder="search"></input>
         </ul>
       </div>
-      <img src={bg_image} width={1600} height={770}></img>
+
+      <div class="contents">
+        <div class="grid-container">
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+          <div class="grid-item"><img src={logo} alt="Logo" /></div>
+        </div>
+      </div>
     </div>
   );
 }
