ead4843b2654c6ed8b94be673b2860f24630b1dc
sokdenspace <sokdenspace@gmail.com>
2022-06-11 19:01:46 +0700

    update

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::resource('products', ProductController::class);
-
-//Route::get('/products', [ProductController::class, 'index']);
-//Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
